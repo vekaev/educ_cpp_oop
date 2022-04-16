b252c30188fdde5bac2f64ac274bc0ce93e7b706
ypetrenko <vekaev4@gmail.com>
2022-04-16 18:25:21 +0300

    Initial Commit

---
+++ educ_cpp_oop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  educ_cpp_oop
+//
+//  Created by Yevhenii Petrenko on 16.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
