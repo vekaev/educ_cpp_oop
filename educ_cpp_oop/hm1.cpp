9506bc180f6760df7870b0b66dec60dfbb9c094e
ypetrenko <vekaev4@gmail.com>
2022-07-20 08:18:40 +0200

    remove this and replace direct settings by setter

--- educ_cpp_oop/hm1.cpp
+++ educ_cpp_oop/hm1.cpp
@@ -3,6 +3,105 @@
 
 using namespace std;
 
+class Number
+{
+protected:
+    int n;
+    int d;
+
+    void set(int _n, int _d = NULL)
+    {
+        n = _n;
+
+        if (_d) {
+            d = _d;
+        }
+        
+        this->normalize();
+    }
+public:
+    Number(int _n, int _d = 1)
+    {
+        this->set(_n, _d);
+    }
+
+    bool isInt()
+    {
+        return d == 1;
+    }
+
+    void Print()
+    {
+        if (this->isInt())
+            cout << n << endl;
+        else
+            if (n > d)
+            cout << n << '/' << d << endl;
+    }
+
+    int getNumerator()
+    {
+        return n;
+    }
+
+    int getDenominator()
+    {
+        return d;
+    }
+
+    void add(Number num)
+    {
+        int num_n = num.getNumerator();
+        int num_d = num.getDenominator();
+
+        if (num.isInt())
+        {
+            int newN = n + num_n * d;
+            
+            this->set(newN);
+        }
+        else
+        {
+            int newN = (n * num_d) + (num_n * d);
+            int newD = d * num_d;
+            
+            this->set(newN, newD);
+        }
+    }
+
+    void normalize()
+    {
+        if (!this->isInt())
+        {
+            if (n == d && n != 1)
+            {
+                this->set(1, 1);
+            }
+            // TODO: write more
+        }
+    }
+};
+
+void hm1()
+{
+    Number n1(5);
+    
+    int five = 5;
+    n1.add(five);
+    n1.Print(); // 10
+    
+    bool one = true;
+    n1.add(one);
+    n1.Print(); // 11
+    
+    Number n2(1, 4);
+    n1.add(n2);
+    n1.Print(); // 45/4
+}
+
+
+// NOTES!!!
+
 // class Number;
 
 // class FractionalNumber : public Number
@@ -24,87 +123,3 @@ using namespace std;
 //         n += num.getNumerator() * d;
 //     }
 // };
-
-class Number
-{
-protected:
-    int n;
-    int d;
-
-public:
-    Number(int _n, int _d = 1)
-    {
-        n = _n;
-        d = _d;
-    }
-
-    bool isInt()
-    {
-        return d == 1;
-    }
-
-    void Print()
-    {
-        if (this->isInt())
-            cout << n << endl;
-        else
-            cout << n << '/' << d << endl;
-    }
-
-    int getNumerator()
-    {
-        return n;
-    }
-
-    int getDenominator()
-    {
-        return d;
-    }
-
-    void add(Number num)
-    {
-        int num_n = num.getNumerator();
-        int num_d = num.getDenominator();
-
-        if (num.isInt())
-        {
-            n += num_n * d;
-        }
-        else
-        {
-            n = (n * num_d) + (num_n * d);
-            d *= num_d;
-        }
-
-        this->normalize();
-    }
-
-    void normalize()
-    {
-        if (!this->isInt())
-        {
-            if (n == d)
-            {
-                n = 1;
-                d = 1;
-            }
-            else if (n > d)
-            {
-            }
-        }
-    }
-
-    // void add(FractionalNumber num)
-    // {
-    //     n = n * num.getDenominator() + num.getNumerator();
-    //     d = num.getDenominator();
-    // }
-};
-
-void hm1()
-{
-    Number n1(5);
-    Number n2(1, 4);
-    n1.add(n2);
-    n1.Print();
-}
